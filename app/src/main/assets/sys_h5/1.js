3b12be68dbda0c544bb1f4b0de3bdf4a018050a6
hanjianqiao <hanjianqiao@gmail.com>
2017-04-10 18:49:04 +0800

    improve: show message and bill list

--- app/src/main/assets/sys_h5/1.js
+++ app/src/main/assets/sys_h5/1.js
@@ -1,25 +1,55 @@
 var end = '<a><div class="fl mr20"><img src="icon03.png" alt=""></div><div class="fl"><p>'
-var end1 = '</p><span>立即查看</span></div></a>';
+var end1 = '</p><span>'
+var end2 = '</span></div><span style="float:right;color:#00c30e">立即查看</span></a>';
+var nowLoad = 0;
+var uid;
+var passwd;
+var target;
 function showDetail(){
     LanJsBridge.setPara3(this.id);
     window.location.href = 'news.html';
 }
+function lastStage0(para){
+    sel = document.getElementById('pull2up');
+    sel.innerHTML = '点击加载更多';
+}
+function lastStage1(para){
+    sel = document.getElementById('pull2up');
+    sel.innerHTML = '已显示全部消息';
+    sel.onclick = '';
+}
 function callBack(html, url){
-	var target = document.getElementsByClassName('bill')[0];
 	var obj = eval('('+html+')');
 	var message = obj.message;
-	for(var i = 0; i < message.length; i++){
-	    var item = document.createElement('li');
-	    var att = document.createAttribute('class');
-	    att.value = 'bill01';
-	    item.id = message[i].id;
-	    item.onclick = showDetail;
-	    item.setAttributeNode(att);
-	    item.innerHTML = end+message[i].title+end1;
-	    target.insertBefore(item, target.childNodes[0]);
-	}
+	if(message.length > 0){
+        for(var i = message.length-1; i >= 0; i--){
+            var item = document.createElement('li');
+            var att = document.createAttribute('class');
+            att.value = 'bill01';
+            item.id = message[i].id;
+            item.onclick = showDetail;
+            item.setAttributeNode(att);
+            item.innerHTML = end+message[i].title+end1+message[i].date+end2;
+            target.appendChild(item);
+        }
+	        //LanJsBridge.callInMain("lastStage0", "");
+            lastStage0('');
+        }else{
+            //LanJsBridge.callInMain("lastStage1", "");
+            lastStage1('');
+        }
 }
 function updateDisplay(userId, password, messageId){
-	htmlText = LanJsBridge.getDataFromUrl("http://user.vsusvip.com:30000/list?userid="+userId+"&password="+password, "callBack");
+    target = document.getElementsByClassName('bill')[0];
+    uid = userId;
+    passwd = password;
+    var limit = 20;
+	htmlText = LanJsBridge.getDataFromUrl("http://user.vsusvip.com:30000/list?userid="+userId+"&password="+password+"&offset="+nowLoad+"&limit="+limit, "callBack");
 	callBack(htmlText, '');
+}
+function loadMore(){
+    var limit = 20;
+    nowLoad = nowLoad+limit;
+	htmlText = LanJsBridge.getDataFromUrl("http://user.vsusvip.com:30000/list?userid="+uid+"&password="+passwd+"&offset="+nowLoad+"&limit="+limit, "callBack");
+    callBack(htmlText, "");
 }
--- app/src/main/assets/sys_h5/5.js
+++ app/src/main/assets/sys_h5/5.js
@@ -2,20 +2,50 @@ var str0 = '<div class="top"><span class="fl top-left">';
 var str1 = '</span><span class="fr green">';
 var str2 = '</span></div><div class="btm"><span class="fl mr03">';
 var str3 = '</span><span class="fr">成功</span>';
+var nowLoad = 0;
+var uid;
+var passwd;
+var target;
+function lastStage0(para){
+    sel = document.getElementById('pull2up');
+    sel.innerHTML = '点击加载更多';
+}
+function lastStage1(para){
+    sel = document.getElementById('pull2up');
+    sel.innerHTML = '已显示全部账单';
+    sel.onclick = '';
+}
 function callBack(html, url){
 	var target = document.getElementsByClassName('bill')[0];
 	var obj = eval('('+html+')');
 	var message = obj.message;
-	for(var i = 0; i < message.length; i++){
-	    var item = document.createElement('li');
-	    var att = document.createAttribute('class');
-	    att.value = 'bill01';
-	    item.setAttributeNode(att);
-	    item.innerHTML = str0+message[i].action+str1+message[i].amount+str2+message[i].date+str3;
-	    target.insertBefore(item, target.childNodes[0]);
-	}
+	if(message.length > 0){
+        for(var i = message.length-1; i >= 0; i--){
+            var item = document.createElement('li');
+            var att = document.createAttribute('class');
+            att.value = 'bill01';
+            item.setAttributeNode(att);
+            item.innerHTML = str0+message[i].action+str1+message[i].amount+str2+message[i].date+str3;
+            target.appendChild(item);
+        }
+            //LanJsBridge.callInMain("lastStage0", "");
+            lastStage0('');
+        }else{
+            //LanJsBridge.callInMain("lastStage1", "");
+            lastStage1('');
+        }
 }
 function updateDisplay(userId, password, messageId){
-	htmlText = LanJsBridge.getDataFromUrl("http://user.vsusvip.com:40000/list?userid="+userId+"&password="+password, "callBack");
-    callBack(htmlText, '');
+    target = document.getElementsByClassName('bill')[0];
+    uid = userId;
+    passwd = password;
+    var limit = 20;
+	htmlText = LanJsBridge.getDataFromUrl("http://user.vsusvip.com:40000/list?userid="+userId+"&password="+password+"&offset="+nowLoad+"&limit="+limit, "callBack");
+	callBack(htmlText, '');
+}
+function loadMore(){
+    var limit = 20;
+    nowLoad = nowLoad+limit;
+	htmlText = LanJsBridge.getDataFromUrl("http://user.vsusvip.com:40000/list?userid="+uid+"&password="+passwd+"&offset="+nowLoad+"&limit="+limit, "callBack");
+    callBack(htmlText, "");
 }
