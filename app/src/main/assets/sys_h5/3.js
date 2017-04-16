7c5be15e409ce290f40e7fafce345ca4cf7708ab
hanjianqiao <hanjianqiao@gmail.com>
2017-04-16 09:11:17 +0800

    anrome 1.9.2 improve: show notification

--- app/src/main/assets/sys_h5/3.js
+++ app/src/main/assets/sys_h5/3.js
@@ -1,12 +1,21 @@
 function callBack(html, url){
 	var obj = eval('('+html+')');
     var ja = obj.data;
-    document.getElementById("inv").innerHTML=ja.invitation_remain;
-    document.getElementById("ext").innerHTML=ja.extend_remain;
     document.getElementById("tot").innerHTML=ja.invitee_total;
     document.getElementById("vip").innerHTML=ja.invitee_vip;
-    document.getElementById("age").innerHTML=ja.invitee_agent;
-    document.getElementById("tea").innerHTML=ja.team_total;
+    document.getElementById("newvip").innerHTML=ja.newvip;
+    document.getElementById("newextend").innerHTML=ja.newextend;
+    if(ja.level == "vip"){
+        document.getElementById("level").innerHTML = "会员"
+    }else if(ja.level == "agent"){
+        document.getElementById("level").innerHTML = "代理"
+    }else if(ja.level == "golden"){
+        document.getElementById("level").innerHTML = "金牌代理"
+    }else if(ja.level == "cofound"){
+        document.getElementById("level").innerHTML = "联合创始人"
+    }else{
+        document.getElementById("level").innerHTML = "用户"
+    }
 }
 function updateDisplay(userId, password){
 	htmlText = LanJsBridge.getDataFromUrls("https://user.vsusvip.com:10000/query?id="+userId+"&pwd="+password, "callBack");
